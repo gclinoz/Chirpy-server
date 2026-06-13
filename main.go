09f042b4789398c4657f9dbc6da1aa8891957200
gclin <t96994024@gs.ncku.edu.tw>
2026-06-13 10:22:27 +0800

    CH2-L2: Stateful Handlers

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"log"
+	"sync/atomic"
 )
 
 func main() {
 	const port = "8080"
 
+	api := &apiConfig{}
 	fileServer := http.FileServer(http.Dir("."))
-	mux := http.NewServeMux()	
+	fileServerInc := api.middlewareMetricsInc(fileServer)
 
+	mux := http.NewServeMux()	
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerInc))
 	mux.HandleFunc("/healthz", handleHealth)
-	mux.Handle("/app", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/metrics", api.handleCountReq)
+	mux.HandleFunc("/reset", api.handleReset)
 
 	server := &http.Server{
 		Addr:		":" + port,
@@ -27,4 +33,24 @@ func handleHealth (w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits	atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleCountReq(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Hits reset to 0")
 }
